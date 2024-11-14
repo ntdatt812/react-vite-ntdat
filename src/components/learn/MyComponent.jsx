9aad5cd2fcf0f1858ab945838ac77135e178b773
ntdatt812 <ntdat812@gmail.com>
2024-11-15 01:01:00 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,23 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const ntdat = 'thanh dat ' //string
+    // const ntdat = 21 //number
+    // const ntdat = true //boolean
+    // const ntdat = null 
+    // const ntdat = undefined
+    // const ntdat = [1, 2, 3]
+    const ntdat = {
+        name: 'ntdat',
+        age: 21
+
+    }
+
     return (
         <>
             <div style={{ borderRadius: "10px" }}>Đây là component đầu tiên! By Nguyễn Thành Đạt</div>
-            <div className="child">child</div>
+            <div>{console.log("Nguyen Thanh Dat")}</div>
+            <div className="child"> {JSON.stringify(ntdat)}child</div>
         </>
     );
 }
