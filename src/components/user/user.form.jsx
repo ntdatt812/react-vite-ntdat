70af33a24c37307d7b3cebee37277695876696f2
ntdatt812 <ntdat812@gmail.com>
2024-11-15 22:09:58 +0700

    video 70 + 71

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,6 +1,6 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
@@ -8,15 +8,19 @@ const UserForm = () => {
     const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleClick = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName,
-            email,
-            password,
-            phone
+    const handleClick = async () => {
+        const res = await createUserAPI(fullName, email, password, phone)
+        if (res.data) {
+            notification.success({
+                message: "CREATE USER",
+                description: "Thêm mới user thành công!"
+            })
+        } else {
+            notification.error({
+                message: "ERROR",
+                description: JSON.stringify(res.message)
+            })
         }
-        axios.post(URL_BACKEND, data);
     }
 
     return (
