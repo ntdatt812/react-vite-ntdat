6f9925216baf1ef808883dac773181b0b22b712a
ntdatt812 <ntdat812@gmail.com>
2024-11-17 19:37:42 +0700

    video 85

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -29,6 +29,12 @@ const UserTable = ({ dataUsers, loadUser }) => {
 
     const columns = [
         {
+            title: "STT",
+            render: (_, record, index) => {
+                return <>{index + 1}</>
+            }
+        },
+        {
             title: 'Id',
             dataIndex: '_id',
             render: (_, record) => (
